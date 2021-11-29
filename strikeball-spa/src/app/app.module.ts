6b520eafca7e30132540654c1357b69a69efb001
k.khoruzhenko <khoruzhenko.work@gmail.com>
2021-11-30 01:09:28 +0300

    add

--- strikeball-spa/src/app/app.module.ts
+++ strikeball-spa/src/app/app.module.ts
@@ -6,13 +6,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import {MatToolbarModule} from '@angular/material/toolbar';
-
+import {MatGridListModule} from '@angular/material/grid-list';
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
     MatToolbarModule,
+    MatGridListModule,
     BrowserModule,
     AuthModule,
     AppRoutingModule,

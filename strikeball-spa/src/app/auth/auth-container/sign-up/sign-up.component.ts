dc610910d620da57927dc0662b012443d3c4e835
Flash2104 <khoruzhenko.work@gmail.com>
2021-11-30 14:51:24 +0300

    add signup component

---
+++ strikeball-spa/src/app/auth/auth-container/sign-up/sign-up.component.ts
@@ -0,0 +1,53 @@
+import { Component, OnInit } from '@angular/core';
+import {
+  FormControl,
+  FormGroup,
+  FormGroupDirective,
+  NgForm,
+  Validators,
+} from '@angular/forms';
+import { ErrorStateMatcher } from '@angular/material/core';
+import { MatDialog } from '@angular/material/dialog';
+
+@Component({
+  selector: 'app-sign-up',
+  templateUrl: './sign-up.component.html',
+  styleUrls: ['./sign-up.component.scss'],
+})
+export class SignUpComponent implements OnInit {
+  emailFormControl = new FormControl('', [
+    Validators.required,
+    Validators.email,
+  ]);
+  form: FormGroup = new FormGroup({
+    emailFormControl: this.emailFormControl,
+    password: new FormControl('', [
+      Validators.required,
+      Validators.minLength(6),
+    ]),
+    confirmPassword: new FormControl('', [
+      Validators.required,
+      Validators.minLength(6),
+    ]),
+  });
+  matcher = new MyErrorStateMatcher();
+  constructor(public dialog: MatDialog) {}
+
+  ngOnInit(): void {}
+
+  onSubmit(): void {}
+}
+
+export class MyErrorStateMatcher implements ErrorStateMatcher {
+  isErrorState(
+    control: FormControl | null,
+    form: FormGroupDirective | NgForm | null
+  ): boolean {
+    const isSubmitted = form && form.submitted;
+    return !!(
+      control &&
+      control.invalid &&
+      (control.dirty || control.touched || isSubmitted)
+    );
+  }
+}
--- strikeball-spa/src/app/auth/auth.module.ts
+++ strikeball-spa/src/app/auth/auth.module.ts
@@ -6,13 +6,15 @@ import { MatCardModule } from '@angular/material/card';
 import { MatDialogModule } from '@angular/material/dialog';
 import { MatIconModule } from '@angular/material/icon';
 import { MatInputModule } from '@angular/material/input';
+import { MatTabsModule } from '@angular/material/tabs';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { AuthContainerComponent } from './auth-container/auth-container.component';
 import { SignInComponent } from './auth-container/sign-in/sign-in.component';
+import { SignUpComponent } from './auth-container/sign-up/sign-up.component';
 import { AuthRoutingModule } from './auth-routing.module';
 
 @NgModule({
-  declarations: [AuthContainerComponent, SignInComponent],
+  declarations: [AuthContainerComponent, SignInComponent, SignUpComponent],
   imports: [
     MatInputModule,
     MatIconModule,
@@ -23,6 +25,7 @@ import { AuthRoutingModule } from './auth-routing.module';
     MatToolbarModule,
     MatButtonModule,
     MatDialogModule,
+    MatTabsModule,
   ],
 })
 export class AuthModule {}

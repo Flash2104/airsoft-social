3d795ecdd6049c8779726aad49bea5538f3e1b55
Flashy <khoruzhenko.work@gmail.com>
2021-12-08 01:53:12 +0300

    add edit page

--- strikeball-spa/src/app/private/private.module.ts
+++ strikeball-spa/src/app/private/private.module.ts
@@ -1,34 +1,43 @@
-import { CommonModule } from '@angular/common';
-import { NgModule } from '@angular/core';
-import { FormsModule, ReactiveFormsModule } from '@angular/forms';
-import { MatButtonModule } from '@angular/material/button';
-import { MatCardModule } from '@angular/material/card';
-import { MatGridListModule } from '@angular/material/grid-list';
-import { MatIconModule } from '@angular/material/icon';
-import { MatInputModule } from '@angular/material/input';
-import { MatListModule } from '@angular/material/list';
-import { MatSidenavModule } from '@angular/material/sidenav';
-import { RouterModule, Routes } from '@angular/router';
-import { SharedModule } from '../shared/shared.module';
-import { СommandContainerComponent } from './command/command-container.component';
-import { ProfileContainerComponent } from './profile/profile-container.component';
-import { SideNavContainerComponent } from './sidenav-container.component';
-import { MatDividerModule } from '@angular/material/divider';
-import { MatTableModule } from '@angular/material/table';
+import {CommonModule} from '@angular/common';
+import {NgModule} from '@angular/core';
+import {FormsModule, ReactiveFormsModule} from '@angular/forms';
+import {MatButtonModule} from '@angular/material/button';
+import {MatCardModule} from '@angular/material/card';
+import {MatGridListModule} from '@angular/material/grid-list';
+import {MatIconModule} from '@angular/material/icon';
+import {MatInputModule} from '@angular/material/input';
+import {MatListModule} from '@angular/material/list';
+import {MatSidenavModule} from '@angular/material/sidenav';
+import {RouterModule, Routes} from '@angular/router';
+import {SharedModule} from '../shared/shared.module';
+import {СommandContainerComponent} from './command/command-container.component';
+import {ProfileContainerComponent} from './profile/profile-container.component';
+import {SideNavContainerComponent} from './sidenav-container.component';
+import {MatDividerModule} from '@angular/material/divider';
+import {MatTableModule} from '@angular/material/table';
+import {ProfileEditComponent} from './profile/edit/profile-edit.component';
 
 const routes: Routes = [
   {
     path: '',
     children: [
-      { path: '*', redirectTo: '/', pathMatch: 'full' },
+      {path: '*', redirectTo: '/', pathMatch: 'full'},
       {
         path: '',
         component: SideNavContainerComponent,
         children: [
-          { path: '', redirectTo: 'profile', pathMatch: 'full' },
+          {path: '', redirectTo: 'profile', pathMatch: 'full'},
           {
             path: 'profile',
             component: ProfileContainerComponent,
+            // children: [
+            //   // {path: '', redirectTo: '/profile/edit', pathMatch: 'full'},
+            //   {path: 'edit', component: ProfileEditComponent},
+            // ]
+          },
+          {
+            path: 'profile/edit',
+            component: ProfileEditComponent,
           },
           {
             path: 'command',
@@ -49,6 +58,7 @@ const routes: Routes = [
     SideNavContainerComponent,
     ProfileContainerComponent,
     СommandContainerComponent,
+    ProfileEditComponent
   ],
   imports: [
     MatButtonModule,
@@ -67,4 +77,5 @@ const routes: Routes = [
     RouterModule.forChild(routes),
   ],
 })
-export class PrivateModule {}
+export class PrivateModule {
+}
---
+++ strikeball-spa/src/app/private/profile/edit/profile-edit.component.ts
@@ -0,0 +1,52 @@
+/* eslint-disable @typescript-eslint/no-empty-function */
+import {
+  ChangeDetectionStrategy,
+  Component,
+  OnDestroy,
+  OnInit,
+} from '@angular/core';
+import { DomSanitizer } from '@angular/platform-browser';
+import { filter, map, Observable, Subject, takeUntil, tap } from 'rxjs';
+
+import { FormControl, FormGroup, Validators } from '@angular/forms';
+import {IProfileData} from '../../../shared/services/dto-models/profile/profile-data';
+import {ProfileRepository} from '../../../shared/repository/profile.repository';
+
+@Component({
+  selector: 'air-profile-edit',
+  templateUrl: './profile-edit.component.html',
+  styleUrls: ['./profile-edit.component.scss'],
+  changeDetection: ChangeDetectionStrategy.OnPush,
+})
+export class ProfileEditComponent implements OnInit, OnDestroy {
+  private _destroy$: Subject<void> = new Subject<void>();
+
+  public loading$: Observable<boolean> = this._profileRepo.loading$.pipe(
+    takeUntil(this._destroy$)
+  );
+
+  public profile$: Observable<IProfileData | null> =
+    this._profileRepo.profile$.pipe(
+      filter((p) => p != null),
+      map((p) => {
+        const sanitized = this._sanitizer.bypassSecurityTrustResourceUrl(
+          'data:image/png;base64, ' + p?.avatarData
+        );
+        return { ...p, avatarData: sanitized } as IProfileData;
+      }),
+      takeUntil(this._destroy$)
+    );
+
+  constructor(
+    private _profileRepo: ProfileRepository,
+    private _sanitizer: DomSanitizer
+  ) {}
+
+  // eslint-disable-next-line @angular-eslint/no-empty-lifecycle-method
+  ngOnInit(): void {}
+
+  ngOnDestroy(): void {
+    this._destroy$.next();
+    this._destroy$.complete();
+  }
+}
